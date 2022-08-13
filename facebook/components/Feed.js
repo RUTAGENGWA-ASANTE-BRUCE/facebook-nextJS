e50e1c91c089fe3049f97c2d66289621b617aeff
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-13 18:12:56 +0200

    feed adding posts

--- facebook/components/Feed.js
+++ facebook/components/Feed.js
@@ -1,11 +1,13 @@
 import React from "react"
 import  Stories from "./Stories"
 import InputBox from "./InputBox"
+import Posts from "./Posts"
 function Feed(){
     return(
     <div className="flex-grow h-screen pt-6 mr-4 xl:mr-40 overflow-y-auto">
     <Stories />
     <InputBox />
+    <Posts />
     </div>)
 }
 

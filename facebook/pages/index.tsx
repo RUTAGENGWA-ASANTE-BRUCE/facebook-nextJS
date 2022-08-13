c2fb0157bae0c8ad50b207e3dc17f78ae3b4f016
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-13 02:52:53 +0200

    rapping everything

--- facebook/pages/index.tsx
+++ facebook/pages/index.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from 'next'
+import { useSession} from "next-auth/react"
 import Head from 'next/head'
 import Image from 'next/image'
+import Login from "../components/Login"
 import Header from "../components/Header"
+import SideBar from "../components/SideBar"
+import Feed from "../components/Feed"
 const Home: NextPage = () => {
+  const { data: session } = useSession()
+  if(!session) return <Login />;
   return (
     <div className="">
+      <Head>
+        <title>Facebook</title>
+      </Head>
       <Header />
+      <main className="flex">
+        <SideBar />
+        <Feed />
+      </main>
     </div>
   )
 }
 
 export default Home
+

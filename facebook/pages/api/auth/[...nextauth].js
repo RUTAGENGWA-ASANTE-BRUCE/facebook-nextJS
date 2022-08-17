35e383c05627831d5d4fa2d69087de21b31dbd82
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-17 03:26:43 +0200

    enable facebook login

--- facebook/pages/api/auth/[...nextauth].js
+++ facebook/pages/api/auth/[...nextauth].js
@@ -3,10 +3,10 @@ import FacebookProvider from "next-auth/providers/facebook";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-    // }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    }),
     // ...add more providers here
   ],
 })

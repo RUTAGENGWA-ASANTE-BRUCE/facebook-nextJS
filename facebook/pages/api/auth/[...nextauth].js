20ed361c74c467e813dd994cfbe8d69c160c2acb
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-17 03:56:28 +0200

    server errors debugging

--- facebook/pages/api/auth/[...nextauth].js
+++ facebook/pages/api/auth/[...nextauth].js
@@ -1,29 +1,12 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import FacebookProvider from "next-auth/providers/facebook"
-
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
-
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export const authOptions = {
+import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook";
+export default NextAuth({
+  // Configure one or more authentication providers
   providers: [
-   
     FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }),
- 
+    // ...add more providers here
   ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
-    },
-  },
-}
-
-export default NextAuth(authOptions)
+})

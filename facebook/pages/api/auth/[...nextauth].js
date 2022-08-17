6db43d90920b41eb31819ab2feccbd67ccd566d4
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-17 03:54:35 +0200

    new auth options

--- facebook/pages/api/auth/[...nextauth].js
+++ facebook/pages/api/auth/[...nextauth].js
@@ -1,12 +1,29 @@
-import NextAuth from "next-auth"
-import FacebookProvider from "next-auth/providers/facebook";
-export default NextAuth({
-  // Configure one or more authentication providers
+import NextAuth, { NextAuthOptions } from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook"
+
+// import AppleProvider from "next-auth/providers/apple"
+// import EmailProvider from "next-auth/providers/email"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export const authOptions = {
   providers: [
+   
     FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
     }),
-    // ...add more providers here
+ 
   ],
-})
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+}
+
+export default NextAuth(authOptions)

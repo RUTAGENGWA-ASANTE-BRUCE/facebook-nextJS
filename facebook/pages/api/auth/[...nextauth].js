a11884548ff66597662a701399171dda86da0d42
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-08-17 03:03:47 +0200

    new changes to logs

--- facebook/pages/api/auth/[...nextauth].js
+++ facebook/pages/api/auth/[...nextauth].js
@@ -4,9 +4,9 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     FacebookProvider({
-      clientId: "828776021444029",
-      clientSecret: "a361995e2a3ceab60ef255f8ad0d2aea"
-    })
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    }),
     // ...add more providers here
   ],
 })
